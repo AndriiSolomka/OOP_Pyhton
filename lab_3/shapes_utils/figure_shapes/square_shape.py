6a7b67d112dedf93c56fb7d34766b48a9608f551
Solomka Andrii <andriissolomka@gmail.com>
2024-10-27 19:25:38 +0200

    add lab_4

--- lab_3/shapes_utils/figure_shapes/square_shape.py
+++
@@ -1,8 +0,0 @@
-from lab_3.shapes_utils.shape.shape import Shape
-
-
-class SquareShape(Shape):
-    def show(self):
-        self.canvas.create_rectangle(
-            self._x1, self._y1, self._x2, self._y2, outline="black", fill="blue"
-        )
--- lab_3/shapes_utils/temp_shapes/temp_square.py
+++
@@ -1,9 +0,0 @@
-from lab_3.shapes_utils.shape.shape import Shape
-
-
-class TempSquare(Shape):
-
-    def show(self):
-        return self.canvas.create_rectangle(
-            self._x1, self._y1, self._x2, self._y2, outline="grey", dash=(4, 2)
-        )
